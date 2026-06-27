85d218ad2c36e11802accfbfec4196914df48538
AmirHosein <karimzade.ah@gmail.com>
2026-06-27 06:13:31 +0330

    .

--- Back/routes/habits.js
+++ Back/routes/habits.js
@@ -4,14 +4,10 @@ const {
   updateHabit,
   createHabit,
   deleteHabit,
-<<<<<<< HEAD
   getTestSort,
   tickHabit,
-} = require("../controllers/habitController");
-=======
   toggleHabitDate,
-} = require("../controllers/habbitController");
->>>>>>> 3a6bb7e2eeeeee0b464fccf2ff2e60926a65c84c
+} = require("../controllers/habitController");
 const authMiddleware = require("../middlewares/auth");
 const router = express.Router();
 
@@ -25,11 +21,4 @@ router.delete("/:id", deleteHabit);
 router.get("/test-sorted", getTestSort);
 router.post("/:id/tick", tickHabit);
 
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> 3a6bb7e2eeeeee0b464fccf2ff2e60926a65c84c
 module.exports = router;
